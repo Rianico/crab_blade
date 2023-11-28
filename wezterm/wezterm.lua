62808303aa4a6d97dda8a7da753daebb2f917ab2
zkx <zhxuankun@163.com>
2023-11-28 10:12:57 +0800

    [wezterm] add configuration about windows

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,39 +1,39 @@
+-- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
+-- This table will hold the configuration.
 local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+  config.default_domain = 'WSL:Ubuntu-22.04'
+end
+
+-- gpu
+local os_name = string.lower(os.getenv("OS") or "")
+if os_name:match("windows") then
+    config.webgpu_power_preference = 'HighPerformance'
+elseif os_name:match("mac") then
+    config.webgpu_power_preference = 'LowPower'
+end
+
 
 config.color_scheme = 'Hardcore'
 
--- font
-config.font = wezterm.font 'JetBrainsMonoNL Nerd Font Mono'
+-- cursor
+config.default_cursor_style = 'SteadyUnderline'
+
+-- This is where you actually apply your config choices
+config.font = wezterm.font 'FiraCode Nerd Font'
 config.font_size = 21.0
 
 config.initial_rows = 47
 config.initial_cols = 199
+
 -- apparance
-config.window_background_opacity = 1.0
-
-config.window_frame = {
-    -- The font used in the tab bar.
-    -- Roboto Bold is the default; this font is bundled
-    -- with wezterm.
-    -- Whatever font is selected here, it will have the
-    -- main font setting appended to it to pick up any
-    -- fallback fonts you may have used there.
-    font = wezterm.font { family = 'Roboto', weight = 'Bold' },
-
-    -- The size of the font in the tab bar.
-    -- Default to 10.0 on Windows but 12.0 on other systems
-    font_size = 18.0,
-
-    -- The overall background color of the tab bar when
-    -- the window is focused
-    active_titlebar_bg = '#333333',
-
-    -- The overall background color of the tab bar when
-    -- the window is not focused
-    inactive_titlebar_bg = '#333333',
-}
+config.window_background_opacity = 0.93
 
 config.window_background_gradient = {
     -- Can be "Vertical" or "Horizontal".  Specifies the direction
@@ -152,4 +152,5 @@ config.quick_select_patterns = {
     '[0-9a-zA-Z]{3,40}',
 }
 
+-- and finally, return the configuration to wezterm
 return config
